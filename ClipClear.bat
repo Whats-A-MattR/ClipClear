c9b7a4e243f60749ca9fcbf5ecfc76f2435c24fa
Matthew Russell <matthew.russell@adit.net.au>
2019-08-21 16:36:07 +1000

    Update in ClipClear.bat
    Updated ClearClip.bat to use WScript instead of CScript to reduce window clutter.

--- ClipClear.bat
+++ ClipClear.bat
@@ -11,5 +11,5 @@ echo off | Clip
 :: to remove the popup, add two colons before lines 12, 13, 14 and 15.
 echo set WshShell = WScript.CreateObject("WScript.Shell") > %tmp%\ClearClipSuccess.vbs
 echo WScript.Quit (WshShell.Popup( "Clipboard has been cleaned." ,1 ,"Clipboard Cleared!", 0)) >> %tmp%\ClearClipSuccess.vbs
-cscript /nologo %tmp%\ClearClipSuccess.vbs
+wscript /nologo %tmp%\ClearClipSuccess.vbs
 del tmp%\ClearClipSuccess.vbs
